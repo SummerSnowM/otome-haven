8a3bee784eb9c6bd8c4f765c302802366c477302
Ng Poh Ting <sumsnow2@gmail.com>
2025-10-11 19:26:33 +0800

    added character update functionality

--- src/components/characters/CharacterCard.jsx
+++ src/components/characters/CharacterCard.jsx
@@ -1,13 +1,21 @@
 import { Card, Button } from 'react-bootstrap';
+import { useState } from 'react';
+
+import UpdateCharacter from './UpdateCharacter';
 
 export default function CharacterCard({ character, imageUrl }) {
+    const [showModal, setShowModal] = useState(false);
+
+    const handleOpenModal = () => setShowModal(true);
+    const handleCloseModal = () => setShowModal(false);
+
     return (
         <>
             <Card className='mt-3'>
                 <Card.Body>
-                    <Card.Img className='img-fluid rounded-4' style={{ height: '340px'}} src={imageUrl}></Card.Img>
+                    <Card.Img className='img-fluid rounded-4' style={{ height: '340px' }} src={imageUrl}></Card.Img>
                     <Card.Text className='mt-2 text-center'><strong>{character.name}</strong></Card.Text>
-                    <Button style={{ backgroundColor: '#E6B2BA', border: 'transparent' }} className='m-1'>
+                    <Button onClick={() => handleOpenModal()} style={{ backgroundColor: '#E6B2BA', border: 'transparent' }} className='m-1'>
                         <i class="bi bi-pencil-fill"></i>
                     </Button>
                     <Button style={{ backgroundColor: '#E6B2BA', border: 'transparent' }} className='m-1'>
@@ -15,6 +23,8 @@ export default function CharacterCard({ character, imageUrl }) {
                     </Button>
                 </Card.Body>
             </Card>
+
+            <UpdateCharacter showModal={showModal} closeModal={handleCloseModal} character={character} />
         </>
     )
 }
---
+++ src/components/characters/UpdateCharacter.jsx
@@ -0,0 +1,106 @@
+import { Modal, Form, Button } from 'react-bootstrap';
+import { useState } from 'react';
+
+import { BASE_URL } from '../../App';
+import axios from 'axios';
+
+import Notification from '../Notification';
+
+export default function UpdateCharacter({ showModal, closeModal, character }) {
+    const [voiceActor, setVoiceActor] = useState(character.voice_actor);
+    const [personality, setPersonality] = useState(character.personality);
+    const [review, setReview] = useState(character.review);
+    const [rating, setRating] = useState(character.rating);
+
+    const [showToast, setShowToast] = useState(false);
+    const [message, setMessage] = useState("");
+
+    const handleOpenToast = () => setShowToast(true);
+    const handleCloseToast = () => setShowToast(false);
+
+    const handleSubmit = () => {
+        const data = {
+            voice_actor: voiceActor,
+            personality,
+            review,
+            rating
+        }
+        axios.put(`${BASE_URL}/characters/${character.id}`, data)
+            .then((response) => setMessage(response.data.message))
+            .catch((error) => console.error(error));
+
+        //close modal
+        closeModal();
+
+        //show toast
+        handleOpenToast();
+    }
+    return (
+        <>
+            <Modal
+                show={showModal}
+                onHide={closeModal}
+                animation={true}
+                centered
+            >
+                <Modal.Header style={{ backgroundColor: '#E6B2BA' }} closeButton>
+                    <Modal.Title>Update Character</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form onSubmit={(e) => {
+                        e.preventDefault();
+                        handleSubmit();
+                    }}>
+                        <Form.Group >
+                            <Form.Label>Voice Actor</Form.Label>
+                            <Form.Control
+                                type='text'
+                                value={voiceActor}
+                                onChange={e => setVoiceActor(e.target.value)}
+                                required
+                            />
+                        </Form.Group>
+
+                        <Form.Group>
+                            <Form.Label>Personality</Form.Label>
+                            <Form.Control
+                                type='text'
+                                value={personality}
+                                onChange={e => setPersonality(e.target.value)}
+                                required
+                            />
+                        </Form.Group>
+
+                        <Form.Group >
+                            <Form.Label>Your Review / Opinion:</Form.Label>
+                            <Form.Control
+                                as='textarea'
+                                rows={5}
+                                value={review}
+                                onChange={e => setReview(e.target.value)}
+                                required
+                            />
+                        </Form.Group>
+
+                        <Form.Group>
+                            <Form.Label>Rating</Form.Label>
+                            <Form.Control
+                                type='number'
+                                max={5}
+                                min={1}
+                                step={0.5}
+                                value={rating}
+                                onChange={e => setRating(e.target.value)}
+                                required
+                            />
+                        </Form.Group>
+
+                        <Button className='w-100 mt-3 rounded-5' style={{ backgroundColor: '#E6B2BA', border: 'transparent', color: 'white' }} type='submit'>Update Character</Button>
+                    </Form>
+                </Modal.Body>
+            </Modal>
+
+            <Notification message={message} closeToast={handleCloseToast} showToast={showToast} />
+        </>
+    )
+}
